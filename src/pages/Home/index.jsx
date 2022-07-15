ed44f05f4cc3b77d107dfbf9cc6b3a6e74cd6c97
HelithaSri <helitha.pravin@gmail.com>
2022-07-15 11:36:46 +0530

    Update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import {Component} from "react";
 import {styleSheet} from "./style";
 import {withStyles} from "@mui/styles";
+import Button from "@mui/material/Button";
+import {Link} from "react-router-dom";
 
 class Home extends Component {
     constructor(props) {
@@ -9,7 +11,29 @@ class Home extends Component {
 
     render() {
         const {classes} = this.props;
-        return (<div>Hello World</div>)
+        return (
+            <div className={classes.container}>
+            <Link className={classes.link} to={"/login"}>
+                <Button variant="contained" size="large">Login</Button>
+            </Link>
+
+            <Link className={classes.link} to={"/dashboard"}>
+                <Button variant="contained" size="large">Dashboard</Button>
+            </Link>
+
+            <Link className={classes.link} to={"/customer"}>
+                <Button variant="contained" size="large">Customer</Button>
+            </Link>
+
+            <Link className={classes.link} to={"/item"}>
+                <Button variant="contained" size="large">Item</Button>
+            </Link>
+
+            <Link className={classes.link} to={"/*"}>
+                <Button variant="contained" size="large">Not Found</Button>
+            </Link>
+        </div>
+        )
     }
 }
 
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,3 +1,14 @@
 export const styleSheet ={
-    
+
+    container:{
+        display:'flex',
+        height:'100vh',
+        width:'100vw',
+        gap:'50px',
+        justifyContent:'center',
+        alignItems:'center',
+        flexDirection:'column',
+        alignContent:'space-around'
+    }
+
 }
